f87835517dac0f16e8497448c54ef832df84bbd6
farahmc <farah.mccurdy@gmail.com>
2022-03-14 16:00:30 -0700

    added print_hex function

--- print_integer.c
+++ print_integer.c
@@ -88,3 +88,46 @@ int print_integer(va_list list)
 		return (number_length(n) + 1);
 	}
 }
+
+/**
+ * print_hex- convert a given unsigned int to hexadecimal (caps)
+ * @list: parameter from va_list
+ * Description: convert a given unsigned int to hexadecimal
+ *(capital letters ABCDEF)
+ * Return: number of integers
+ */
+
+int print_hex(va_list list)
+{
+	unsigned int num, remainder;
+	int i, j = 0;
+	char hexdec[8];
+
+	num = va_arg(list, unsigned int);
+
+	while (num != 0)
+	{
+		remainder = num % 16;
+		if (remainder < 10)
+		{
+			hexdec[j] = 48 + remainder;
+			j++;
+		}
+		else
+		{
+			hexdec[j] = 55 + remainder;
+			j++;
+		}
+		num = num / 16;
+		j++;
+	}
+
+	/* print in reverse */
+	i = j - 1;
+	while (i >= 0)
+	{
+		_putchar(hexdec[i]);
+		i--;
+	}
+	return (j);
+}
