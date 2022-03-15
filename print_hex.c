a783cc3dc5a8d1c065b978484423b41cc78fad64
farahmc <farah.mccurdy@gmail.com>
2022-03-14 17:00:04 -0700

    new file for print_hex and print_hex_lowercase

---
+++ print_hex.c
@@ -0,0 +1,83 @@
+/**
+ * print_hex- convert a given unsigned int to hexadecimal (caps)
+ * @list: parameter from va_list
+ * Description: convert a given unsigned int to hexadecimal
+ *(capital letters ABCDEF)
+ * Return: number of integers
+ */
+
+int print_hex(va_list list)
+{
+	unsigned int num, remainder;
+	int i, j = 0;
+	char hexdec[50];
+
+	num = va_arg(list, unsigned int);
+
+	while (num != 0)
+	{
+		remainder = num % 16;
+		if (remainder < 10)
+		{
+			hexdec[j] = 48 + remainder;
+			j++;
+		}
+		else
+		{
+			hexdec[j] = 55 + remainder;
+			j++;
+		}
+		num = num / 16;
+	}
+
+	/* print in reverse */
+	i = j - 1;
+	while (i >= 0)
+	{
+		_putchar(hexdec[i]);
+		i--;
+	}
+	return (number_length(hexdec[i]));
+}
+
+/**
+ * print_hex_lower- convert a given unsigned int to hexadecimal (lower)
+ * @list: parameter from va_list
+ * Description: convert a given unsigned int to hexadecimal
+ *(lowercase letters abcdef)
+ * Return: number of integers
+ */
+
+int print_hex_lower(va_list list)
+{
+	unsigned int num, remainder;
+	int i, j = 0;
+	char hexdec[50];
+
+	num = va_arg(list, unsigned int);
+
+	while (num != 0)
+	{
+		remainder = num % 16;
+		if (remainder < 10)
+		{
+			hexdec[j] = 48 + remainder + 32;
+			j++;
+		}
+		else
+		{
+			hexdec[j] = 55 + remainder + 32;
+			j++;
+		}
+		num = num / 16;
+	}
+
+	/* print in reverse */
+	i = j;
+	while (i >= 0)
+	{
+		_putchar(hexdec[i]);
+		i--;
+	}
+	return (number_length(hexdec[i]));
+}
